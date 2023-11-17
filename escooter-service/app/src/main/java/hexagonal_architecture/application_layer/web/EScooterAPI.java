468914c78522dcb9d634d93ac96bf5593e1ee328
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2023-11-17 20:33:19 +0100

    add api-escooter-service with OpenAPI

--- escooter-service/app/src/main/java/hexagonal_architecture/application_layer/web/EScooterAPI.java
+++ escooter-service/app/src/main/java/hexagonal_architecture/application_layer/web/EScooterAPI.java
@@ -18,26 +18,31 @@ public class EScooterAPI {
     @Autowired
     private DataService<EScooter, String> escooterService;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("")
     public ResponseEntity<List<EScooter>> getAllEScooters() {
         return ResponseEntity.ok().body(this.escooterService.getAllResources());
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/{id}")
     public ResponseEntity<EScooter> getEScooterById(@PathVariable String id) {
         return ResponseEntity.ok().body(this.escooterService.getResourceById(id));
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("")
     public ResponseEntity<EScooter> createEScooter(EScooter escooter) throws IdAlreadyExistingException, NotYetRegisteredException {
         return ResponseEntity.ok().body(this.escooterService.createResource(escooter));
     }
 
+    @CrossOrigin(origins = "*")
     @PutMapping("/{id}")
     public ResponseEntity<EScooter> updateEScooter(@PathVariable String id, @RequestBody EScooter escooter) {
         return ResponseEntity.ok().body(this.escooterService.updateResource(id, escooter));
     }
 
+    @CrossOrigin(origins = "*")
     @DeleteMapping("/{id}")
     public HttpStatus deleteEScooter(@PathVariable String id) {
         this.escooterService.deleteResource(id);
